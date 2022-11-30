34e04f327aa6d0ad9c10b8be792c2e912836f9f2
Anant Dubey <anantkumar312@gmail.com>
2022-12-01 02:25:38 +0530

    added predict code to app.R

--- cryptoProject/app.R
+++ cryptoProject/app.R
@@ -1,43 +1,73 @@
+#install.packages("fpp2")
+
 library(shiny)
 
 ui <- fluidPage(
-  navbarPage("Crypto Data",
+  navbarPage("Cryptocurrency Data",
              tabPanel("Single",
-  titlePanel("Cryptocurrency"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      #Dropdown menu for selecting cryptocurrency
-      selectInput("select_crypto", "Select Cryptocurrency", choices = c("Bitcoin", "Ethereum", "Doge_Coin")),
-      # Displaying x and y co-ordinates
-      verbatimTextOutput("info"),
-      
-      # Select whether to overlay smooth trend line
-      checkboxInput(inputId = "smoother", label = strong("Overlay smooth trend line"), value = FALSE),
-      
-      # Display only if the smoother is checked
-      conditionalPanel(condition = "input.smoother == true",
-                       sliderInput(inputId = "f", label = "Smoother span:",
-                                   min = 0.01, max = 1, value = 0.67, step = 0.01,
-                                   animate = animationOptions(interval = 100)),
-                       HTML("Higher values give more smoothness.")
-      )
-    ),
-    mainPanel(
-      # Displaying plot graph
-      plotOutput("lineplot", click = "plot_click")
-    )
-  )
+                      sidebarLayout(
+                        sidebarPanel(
+                          #Dropdown menu for selecting cryptocurrency
+                          selectInput("select_crypto", "Select Cryptocurrency", choices = c("Bitcoin", "Ethereum", "Doge_Coin")),
+                          # Displaying x and y co-ordinates
+                          verbatimTextOutput("info"),
+                          
+                          # Select whether to overlay smooth trend line
+                          checkboxInput(inputId = "smoother", label = strong("Overlay smooth trend line"), value = FALSE),
+                          
+                          # Display only if the smoother is checked
+                          conditionalPanel(condition = "input.smoother == true",
+                                           sliderInput(inputId = "f", label = "Smoother span:",
+                                                       min = 0.01, max = 1, value = 0.67, step = 0.01,
+                                                       animate = animationOptions(interval = 100)),
+                                           HTML("Higher values give more smoothness.")
+                          )
+                        ),
+                        mainPanel(
+                          # Displaying plot graph
+                          plotOutput("lineplot", click = "plot_click")
+                        )
+                      )
              ),
-  tabPanel("Compare")
+             
+             tabPanel("Compare",
+                      sidebarLayout(
+                        sidebarPanel(),
+                        mainPanel()
+                      )
+             ),
+             
+             tabPanel("Prediction",
+                      sidebarLayout(
+                        sidebarPanel(
+                          # select time range to display 
+                          sliderInput("n", "Number of Days",
+                                      value = c(153, 253),
+                                      min = 1,
+                                      max = 253
+                          ),
+                          # days for prediction ahead
+                          numericInput("h", "Days to predict", value = 10),
+                          
+                          # add options for prediction method
+                          radioButtons("model", "Model to select",
+                                       choices = c("ARIMA", "NeuralNet"),
+                                       choiceValues = "ARIMA")
+                          
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          plotOutput("trendPlot")
+                          
+                        )
+                      )
+             )
   )
 )
 
 
-
 server <- function(input, output) {
-  
-  
   
   # Create scatter-plot
   output$lineplot <- renderPlot({
@@ -61,11 +91,29 @@ server <- function(input, output) {
       smooth_curve <- lowess(x = as.numeric(selectedInput$Date), y = selectedInput$Close, f = input$f)
       lines(smooth_curve, col = "#E6553A", lwd = 3)
     }
-    })
+  })
   
   # Display x co-ordinate and price
   output$info <- renderText({
     paste0("x = ", input$plot_click$x, "\nClosing Price = ", input$plot_click$y, "0$")
+  })
+  
+  # Display prediction
+  output$trendPlot <- renderPlot({
+    library(fpp2)
+    
+    stock <- Bitcoin
+    
+    end = dim(stock)[1]
+    start = end - 100
+    
+    if (input$model == "ARIMA"){
+      mod <- auto.arima(stock[start : end, "Close"])
+    } else {
+      mod <- nnetar(stock[start : end, "Close"])
+    }
+    data <- forecast(mod, h = input$h)
+    autoplot(forecast(mod, h = input$h)) + ggtitle("Forecast for next 10 Days based on past 100 Days Price")
   })
 }
 
