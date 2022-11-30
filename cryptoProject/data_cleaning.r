30ce9b123f3ac4f4c612fe132090a1c879c5474f
Anant Dubey <anantkumar312@gmail.com>
2022-11-30 23:00:53 +0530

    corrected df to Bitcoin

--- cryptoProject/data_cleaning.r
+++ cryptoProject/data_cleaning.r
@@ -10,16 +10,16 @@ Bitcoin <- data.frame(lapply(bitcoin_data, gsub, pattern='[\\$,]', replacement='
 #Removing left white space by using str_trim() function and looping the same function for every column using lapply
 #install.packages("stringr") # Install the stringr package to use str_trim
 library(stringr)
-Bitcoin <- data.frame(lapply(df, function(x)str_trim(x)))
+Bitcoin <- data.frame(lapply(Bitcoin, function(x)str_trim(x)))
 Bitcoin
 
 Bitcoin[3495,] # printing last row of the data frame
 
-Bitcoin$Open <- as.numeric(df$Open) # Changing string type to numeric
-Bitcoin$High <- as.numeric(df$High) # Changing string type to numeric
-Bitcoin$Low <- as.numeric(df$Low) # Changing string type to numeric
-Bitcoin$Close <- as.numeric(df$Close) # Changing string type to numeric
-Bitcoin$Volume <- as.numeric(df$Volume) # Changing string type to numeric
-Bitcoin$Market.Cap <- as.numeric(df$Market.Cap) # Changing string type to numeric
-Bitcoin$Date <- as.Date(df$Date, format = "%B %d %Y") # Changing date formats
+Bitcoin$Open <- as.numeric(Bitcoin$Open) # Changing string type to numeric
+Bitcoin$High <- as.numeric(Bitcoin$High) # Changing string type to numeric
+Bitcoin$Low <- as.numeric(Bitcoin$Low) # Changing string type to numeric
+Bitcoin$Close <- as.numeric(Bitcoin$Close) # Changing string type to numeric
+Bitcoin$Volume <- as.numeric(Bitcoin$Volume) # Changing string type to numeric
+Bitcoin$Market.Cap <- as.numeric(Bitcoin$Market.Cap) # Changing string type to numeric
+Bitcoin$Date <- as.Date(Bitcoin$Date, format = "%B %d %Y") # Changing date formats
 
