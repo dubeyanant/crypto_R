263a1e589c9d82c5a5ec78b16f5f215b33ed9c09
Anant Dubey <anantkumar312@gmail.com>
2022-11-30 17:45:45 +0530

    added a date feature

--- cryptoProject/app.R
+++ cryptoProject/app.R
@@ -34,18 +34,27 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  
+  # Subset data
+  selected_trends <- reactive({
+    req(input$date)
+    validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+    validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+    df %>%
+      filter(
+        date > as.POSIXct(input$date[1]) & date < as.POSIXct(input$date[2])
+      )
+  })
   
   # Create scatter-plot
   output$lineplot <- renderPlot({
     color = "#434343"
     par(mar = c(4, 4, 1, 1))
-    plot(x = df$Date, y = df$Close, type = "l", xlab = "Date", ylab = "Closing Price in $",
+    plot(x = selected_trends()$date, y = selected_trends()$Close, type = "l", xlab = "Date", ylab = "Closing Price in $",
          col = color, fg = color, col.lab = color, col.axis = color)
     
     # Display only if smoother is checked
     if(input$smoother){
-      smooth_curve <- lowess(x = as.numeric(df$Date), y = df$Close, f = input$f)
+      smooth_curve <- lowess(x = as.numeric(selected_trends()$date), y = selected_trends()$Close, f = input$f)
       lines(smooth_curve, col = "#E6553A", lwd = 3)
     }
   })
