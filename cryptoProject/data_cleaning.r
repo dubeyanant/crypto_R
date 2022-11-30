a293abeb3ac64f2e5ecb1d3ebb60ee504f35a996
Anant Dubey <anantkumar312@gmail.com>
2022-12-01 01:46:25 +0530

    cleaned data_mining.R

--- cryptoProject/data_cleaning.r
+++ cryptoProject/data_cleaning.r
@@ -18,37 +18,10 @@ Bitcoin[,-1] <- data.frame(lapply(Bitcoin[,-1], as.numeric)) # Changing string t
 
 
 #Ethereum Cleaning
-Ethereum <- read.csv(paste(as.character(getwd()),"/Ethereum.csv", sep="")) # Reading csv file
-
-Ethereum # Printing data frame
-
-#Cleaning the data set by removing the $ and ,
+Ethereum <- read.csv(paste(as.character(getwd()),"/Ethereum.csv", sep=""))
 Ethereum <- data.frame(lapply(Ethereum, gsub, pattern='[\\$,]', replacement=''))
-
-#install.packages("stringr")
-library(stringr)
-
-#Removing left white space by using str_trim()
 Ethereum <- data.frame(lapply(Ethereum, function(x)str_trim(x)))
+Ethereum$Date <- as.Date(Ethereum$Date, format = "%B %d %Y")
+Ethereum[,-1] <- data.frame(lapply(Ethereum[,-1], as.numeric))
 
-Ethereum$Date <- as.Date(Ethereum$Date, format = "%B %d %Y") # Changing date formats
-
-Ethereum[,-1] <- data.frame(lapply(Ethereum[,-1], as.numeric)) # Changing string to numeric
-
-#Doge Cleaning
-doge_data <- read.csv(paste(as.character(getwd()),"/Doge Coin.csv", sep="")) # Reading csv file
-
-doge_data # Printing data frame
-
-#Cleaning the data set by removing the $ and ,
-doge_data <- data.frame(lapply(doge_data, gsub, pattern='[\\$,]', replacement=''))
-
-#install.packages("stringr")
-library(stringr)
-
-#Removing left white space by using str_trim()
-doge_data <- data.frame(lapply(doge_data, function(x)str_trim(x)))
-
-doge_data$Date <- as.Date(doge_data$Date, format = "%B %d %Y") # Changing date formats
-
-doge_data[,-1] <- data.frame(lapply(doge_data[,-1], as.numeric)) # Changing string to numeric
+#DogeCoin Cleaning
