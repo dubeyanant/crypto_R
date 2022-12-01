6a0405c31de93f4a5c7ac37b481a106cfc08ba61
Anant Dubey <anantkumar312@gmail.com>
2022-12-02 00:42:42 +0530

    optimized code for data_cleaning.R

--- cryptoProject/data_cleaning.r
+++ cryptoProject/data_cleaning.r
@@ -1,21 +1,12 @@
-#Bitcoin Cleaning
-bitcoin_data <- read.csv(paste(as.character(getwd()),"/Bitcoin.csv", sep="")) # Reading csv file
-
-bitcoin_data # Printing data frame
-
-#Cleaning the data set by removing the $ and ,
-Bitcoin <- data.frame(lapply(bitcoin_data, gsub, pattern='[\\$,]', replacement=''))
-
 #install.packages("stringr")
 library(stringr)
 
-#Removing left white space by using str_trim()
-Bitcoin <- data.frame(lapply(Bitcoin, function(x)str_trim(x)))
-
+#Bitcoin Cleaning
+Bitcoin <- read.csv(paste(as.character(getwd()),"/Bitcoin.csv", sep="")) # Reading csv file
+Bitcoin <- data.frame(lapply(Bitcoin, gsub, pattern='[\\$,]', replacement='')) # Cleaning the data set by removing the $ and ,
+Bitcoin <- data.frame(lapply(Bitcoin, function(x)str_trim(x))) # Removing left white space by using str_trim()
 Bitcoin$Date <- as.Date(Bitcoin$Date, format = "%B %d %Y") # Changing date formats
-
 Bitcoin[,-1] <- data.frame(lapply(Bitcoin[,-1], as.numeric)) # Changing string to numeric
-
 
 #Ethereum Cleaning
 Ethereum <- read.csv(paste(as.character(getwd()),"/Ethereum.csv", sep=""))
@@ -24,4 +15,4 @@ Ethereum <- data.frame(lapply(Ethereum, function(x)str_trim(x)))
 Ethereum$Date <- as.Date(Ethereum$Date, format = "%B %d %Y")
 Ethereum[,-1] <- data.frame(lapply(Ethereum[,-1], as.numeric))
 
-#DogeCoin Cleaning
+#Dogecoin Cleaning
