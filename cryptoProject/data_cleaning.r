76864be6b48ae60571c3d696202d059e37b78e46
Anant Dubey <anantkumar312@gmail.com>
2022-12-02 01:26:58 +0530

    added a column in data sets

--- cryptoProject/data_cleaning.r
+++ cryptoProject/data_cleaning.r
@@ -7,6 +7,7 @@ Bitcoin <- data.frame(lapply(Bitcoin, gsub, pattern='[\\$,]', replacement='')) #
 Bitcoin <- data.frame(lapply(Bitcoin, function(x)str_trim(x))) # Removing left white space by using str_trim()
 Bitcoin$Date <- as.Date(Bitcoin$Date, format = "%B %d %Y") # Changing date formats
 Bitcoin[,-1] <- data.frame(lapply(Bitcoin[,-1], as.numeric)) # Changing string to numeric
+Bitcoin$MarketCap <- Bitcoin$Market.Cap/1000000000
 
 #Ethereum Cleaning
 Ethereum <- read.csv(paste(as.character(getwd()),"/Ethereum.csv", sep=""))
@@ -14,5 +15,6 @@ Ethereum <- data.frame(lapply(Ethereum, gsub, pattern='[\\$,]', replacement=''))
 Ethereum <- data.frame(lapply(Ethereum, function(x)str_trim(x)))
 Ethereum$Date <- as.Date(Ethereum$Date, format = "%B %d %Y")
 Ethereum[,-1] <- data.frame(lapply(Ethereum[,-1], as.numeric))
+Ethereum$MarketCap <- Ethereum$Market.Cap/1000000000
 
 #Dogecoin Cleaning
