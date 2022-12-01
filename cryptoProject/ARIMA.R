bab15cd60f7f9e72861eb43f1e8fa60d4fba85b9
Anant Dubey <anantkumar312@gmail.com>
2022-12-02 00:39:35 +0530

    added ARIMA.R prediciton module

--- cryptoProject/ARIMA.R
+++ cryptoProject/ARIMA.R
@@ -1,6 +1,46 @@
-BTC <-  subset(Bitcoin, select = c(Date, Close))
+BTC <-  subset(Bitcoin, select = c(Date, Close)) # Loading dataset
 
-rm(btctime)
-btctime = ts(BTC$Close, start = min(BTC$Date), end = max(BTC$Date), frequency = 1)
+# ---- Starting aggregating
+library(dplyr)
+BTC <- arrange(BTC, Date) # Arranging Date in ascending order
 
+library(zoo)
+BTC$qdate <- as.yearqtr(BTC$Date) # Deriving quarterly dates
 
+BTC_q <- BTC %>%
+  group_by(qdate) %>%
+  summarise_all(mean) # Grouping quarterly dates
+# ---- Stopped aggregating
+
+BTC_qtrly <-  subset(BTC_q, select = c(qdate, Close)) # Creating new data set
+
+# ---- Starting Time Series
+
+# Creating a time series
+btctime <- ts(BTC_qtrly$Close, start = min(BTC_qtrly$qdate), end = max(BTC_qtrly$qdate), frequency = 4)
+
+library(forecast)
+library(tseries)
+
+#plot(btctime)
+
+# Checking if the data is static
+acf(btctime)
+pacf(btctime)
+adf.test(btctime)
+
+btcmodel <- auto.arima(btctime, ic = "aic", trace = TRUE) # Making the data set more static
+
+btcmodel
+
+# Checking if the data is static
+acf(ts(btcmodel$residuals))
+pacf(ts(btcmodel$residuals))
+
+rm(BTC) + rm(BTC_q) + rm(BTC_qtrly) + rm(btctime) # Removing variables from memory
+
+#mybtcforecast <- forecast(btcmodel, level = c(95), h = 1)
+#mybtcforecast
+#plot(mybtcforecast)
+
+# ---- Stopped Time Series
